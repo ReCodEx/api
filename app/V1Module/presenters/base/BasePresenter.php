0d369813ddac1eb4dcf9bf59bf1bf450183571bd
Simon Rozsival <simon@rozsival.com>
2016-07-24 10:47:30 +0200

    Error handling and automatic HTTP method validation

--- app/V1Module/presenters/base/BasePresenter.php
+++ app/V1Module/presenters/base/BasePresenter.php
@@ -3,11 +3,21 @@
 namespace App\V1Module\Presenters;
 
 use App\Model\Repository\Users;
+use App\Exception\NotFoundException;
+use App\Exception\BadRequestException;
+use App\Exception\ForbiddenRequestException;
+use App\Exception\WrongHttpMethodException;
+
+use Nette\Application\Application;
+use Nette\Reflection;
 
 class BasePresenter extends \App\Presenters\BasePresenter {
   
   /** @var Users */
   protected $users;
+  
+  /** @inject @var Application */
+  public $application;
 
   /**
    * @param Users $users  Users repository
@@ -15,11 +25,24 @@ class BasePresenter extends \App\Presenters\BasePresenter {
   public function __construct(Users $users) {
     $this->users = $users;
   }
+  
+  public function startup() {
+    parent::startup();
+    $this->application->errorPresenter = "V1:ApiError";
+
+    // checks, if the action has allowed method annotation
+    $reflection = new Reflection\ClassType(get_class($this));
+    $actionMethodName = $this->formatActionMethod($this->getAction());
+    $httpMethod = $this->getHttpRequest()->getMethod();
+    if ($reflection->getMethod($actionMethodName)->hasAnnotation($httpMethod) === FALSE) {
+      throw new WrongHttpMethodException($httpMethod);
+    }
+  }
 
   protected function findUserOrThrow(string $id) {
     if ($id === 'me') {
       if (!$this->user->isLoggedIn()) {
-        throw new Exception; // @todo report a 401 error 
+        throw new ForbiddenRequestException; 
       }
 
       $id = $this->user->id;
@@ -27,8 +50,7 @@ class BasePresenter extends \App\Presenters\BasePresenter {
 
     $user = $this->users->get($id);
     if (!$user) {
-      // @todo report a 404 error
-      throw new Exception;
+      throw new NotFoundException;
     }
 
     return $user;
