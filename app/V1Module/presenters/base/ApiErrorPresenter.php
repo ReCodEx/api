92c43f41e77404a8949539fb470cb26d842a3fff
Simon Rozsival <simon@rozsival.com>
2016-07-24 10:46:33 +0200

    Error handling

---
+++ app/V1Module/presenters/base/ApiErrorPresenter.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\V1Module\Presenters;
+
+use App\Exception\ApiException;
+use Nette\Application;
+use Nette\Http\IResponse;
+use Nette\Application\BadRequestException;
+
+/**
+ * The error presenter for the API module - all responses are served as JSONs with a fixed format.
+ */
+class ApiErrorPresenter extends \App\Presenters\BasePresenter {
+
+  /**
+   * @param  Exception
+   * @return void
+   */
+  public function renderDefault($exception) {
+    if ($exception instanceof ApiException) {
+      $this->handleAPIException($exception);
+    } elseif ($exception instanceof BadRequestException) {
+      $this->sendErrorResponse($exception->getCode(), "Bad Request");
+    } else {
+      $this->sendErrorResponse(IResponse::S500_INTERNAL_SERVER_ERROR, "Unexpected Error {$exception}");
+    }
+  }
+
+  /**
+    * Send an error response based on a known type of exceptions - derived from ApiException
+    * @param  ApiException $exception The exception which caused the error
+    */
+  protected function handleAPIException(ApiException $exception) {
+    $this->sendErrorResponse($exception->getCode(), $exception->getMessage());
+  }
+
+  /**
+    * Send a JSON response with a specific HTTP code
+    * @param  int      $code HTTP code of the response
+    * @param  string   $msg  Human readable description of the error
+    * @return void
+    */
+  protected function sendErrorResponse($code, $msg) {
+    $this->getHttpResponse()->setCode($code);
+    $this->sendJson([
+        "code"      => $code,
+        "success"   => FALSE,
+        "msg"       => $msg
+    ]);
+  }
+
+  /**
+    * Log the error respose
+    * @param  Nette\Application\IResponse $response The response information
+    * @return void
+    */
+  protected function shutdown($response) {
+    // @todo log the error
+  }
+
+}
