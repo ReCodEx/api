2ac2353bc42f8857cee2ee5bf4aa2b3fd7f8be5a
Marek Lisy <marek.lisy@centrum.cz>
2016-08-05 21:08:55 +0200

    Little clean-up

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -15,7 +15,6 @@ $configurator->createRobotLoader()
 	->register();
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
-$configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
 $container = $configurator->createContainer();
 
