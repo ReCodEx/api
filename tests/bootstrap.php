cf6b0023fe3f144e2a296ef484508b71099e9f57
Marek Lisy <marek.lisy@centrum.cz>
2016-08-08 17:20:35 +0200

    Revert "Remove local config"
    This reverts commit 2bc8658a134ccdd1a529e7490e0a85a4c0eb0d80.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,4 +12,5 @@ $configurator->createRobotLoader()
 	->register();
 
 $configurator->addConfig(__DIR__ . '/../app/config/config.neon');
+$configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
 return $configurator->createContainer();
