56ff6820ae6d6539d13a5a565c1d2726f3a9c375
Simon Rozsival <simon@rozsival.com>
2016-09-03 21:32:28 +0200

    Score calculator update

--- app/model/helpers/IScoreCalculator.php
+++ app/model/helpers/IScoreCalculator.php
@@ -2,9 +2,7 @@
 
 namespace App\Model\Helpers;
 
-use Doctrine\Common\Collections\ArrayCollection;
-
 interface IScoreCalculator {
-    public function computeScore(string $scoreConfig, ArrayCollection $testResults);
-    public function isScoreConfigValid(string $scoreConfig);
+  public function computeScore(array $testResults);
+  public static function isScoreConfigValid(string $scoreConfig);
 }
