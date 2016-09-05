8b7714e721a3cec9e3ea698e65fb7253187cecea
Simon Rozsival <simon@rozsival.com>
2016-09-05 12:08:34 +0200

    Missing judge output getter

--- app/helpers/EvaluationResults/TaskResults/EvaluationTaskResult.php
+++ app/helpers/EvaluationResults/TaskResults/EvaluationTaskResult.php
@@ -4,7 +4,10 @@ namespace App\Helpers\EvaluationResults;
 use App\Exception\ResultsLoadingException;
 
 class EvaluationTaskResult extends TaskResult {
-    
+
+  /** @var string The output of the judge */
+  private $judgeOutput = "";
+
   /** @var float Explicit score from the results */
   private $score = NULL;
 
@@ -13,7 +16,8 @@ class EvaluationTaskResult extends TaskResult {
 
     // judge output is optional and only the first token is interpreted as float value between 0 and 1
     if (isset($data["judge_output"]) && !empty($data["judge_output"])) {
-      $score = floatval(strtok($data["judge_output"], " "));
+      $this->judgeOutput = $data["judge_output"];
+      $score = floatval(strtok($this->judgeOutput, " "));
       $this->score = min(TaskResult::MAX_SCORE, max(TaskResult::MIN_SCORE, $score));
     }
   }
@@ -24,10 +28,14 @@ class EvaluationTaskResult extends TaskResult {
    */
   public function getScore(): float {
     if ($this->score !== NULL) {
-      return $this->score;      
+      return $this->score;
     }
 
     return parent::getScore();
+  }
+
+  public function getJudgeOutput(): string {
+    return $this->judgeOutput;
   }
 
 }
