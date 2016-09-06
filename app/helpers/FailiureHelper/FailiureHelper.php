663deb9998355f25be8857c12e975b693b48aaec
Simon Rozsival <simon@rozsival.com>
2016-09-06 21:31:01 +0200

    Failiure helper

---
+++ app/helpers/FailiureHelper/FailiureHelper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Helper;
+
+use Nette\Mail\IMailer;
+use Nette\Mail\Message;
+use Nette\Utils\Arrays;
+
+class FailiureHelper {
+
+  const TYPE_BACKEND_ERROR = "BACKEND ERROR";
+  const TYPE_API_ERROR = "API ERROR";
+
+  /** @var EmailHelper */
+  private $mailer;
+
+  /** @var string */
+  private $receivers;
+
+  /** @var string */
+  private $sender;
+
+  /** @var string */
+  private $subjectPrefix;
+
+  public function __construct(EmailHelper $mailer, array $params) {
+    $this->mailer = $mailer;
+    $this->receivers = Arrays::get($params, ["emails", "to"], [ "admin@recodex.org" ]);
+    $this->sender = Arrays::get($params, ["emails", "from"], "noreply@recodex.org");
+    $this->subjectPrefix = Arrays::get($params, ["emails", "subjectPrefix"], "ReCodEx Failiure Report - ");
+
+    if (!is_array($this->receivers)) {
+      $this->receivers = [ $this->receivers ];
+    }
+  }
+
+  public function report(string $type, string $message) {
+    $this->emailHelper->send(
+      $this->sender,
+      $this->receivers,
+      $this->formatSubject($type),
+      $this->formatBody($message)
+    );
+    // @todo: Save the report to the database
+  }
+
+  private function formatSubject(string $type): string {
+    return $type; // @todo
+  }
+
+  private function formatBody(string $message): string {
+    return $message; // @todo
+  }
+
+}
