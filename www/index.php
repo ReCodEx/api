eba9bf7c230c52093a2352c56936ea9ecb17c1cd
Simon Rozsival <simon@rozsival.com>
2016-09-07 13:34:23 +0200

    Cleanup

--- www/index.php
+++ www/index.php
@@ -4,8 +4,5 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: headers, Authorization, Accept-Language");
 
-// Uncomment this line if you must temporarily take down your site for maintenance.
-// require __DIR__ . '/.maintenance.php';
-
 $container = require __DIR__ . '/../app/bootstrap.php';
 $container->getByType(Nette\Application\Application::class)->run();
