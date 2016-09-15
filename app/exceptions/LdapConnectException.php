cb4503f546689bd6cce48faf43172d8cb46dbf8f
Petr Stefan <ptr.stef@gmail.com>
2016-09-16 00:38:04 +0200

    Wrong namespace fix

--- app/exceptions/LdapConnectException.php
+++ app/exceptions/LdapConnectException.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Exception;
+namespace App\Exceptions;
 
 class LdapConnectException extends ApiException {
   public function __construct($msg = 'Cannot connect to LDAP server. Please check your configuration.') {
