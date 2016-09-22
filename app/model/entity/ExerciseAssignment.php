b4aea5492b4c7cce66ddb1c05747dd2c5b3f05ac
Simon Rozsival <simon@rozsival.com>
2016-09-22 20:41:48 +0200

    typos

--- app/model/entity/ExerciseAssignment.php
+++ app/model/entity/ExerciseAssignment.php
@@ -64,12 +64,12 @@ class ExerciseAssignment implements JsonSerializable
    * @return string File path of the 
    */
   public function getJobConfigFilePath(): string {
-    if (!$this->getJobConfigFilePath) {
+    if (!$this->jobConfigFilePath) {
       return $this->getExercise()->getJobConfigFilePath();
     }
 
     // @todo: Make dependable on the programming language/technology used by the user 
-    return $this->jobConfigFIlePath;
+    return $this->jobConfigFilePath;
   }
 
   /**
