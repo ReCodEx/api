1eae79cca2190f68ea9cc67e0c1ad3d6a3d773c1
Martin Polanka <PolankaMartin@gmail.com>
2016-10-19 16:47:09 +0200

    ForgottenPasswordHelper should now do everything needed

--- app/helpers/ForgottenPasswordHelper/ForgottenPasswordHelper.php
+++ app/helpers/ForgottenPasswordHelper/ForgottenPasswordHelper.php
@@ -6,6 +6,7 @@ use Nette\Utils\Arrays;
 use Kdyby\Doctrine\EntityManager;
 use App\Model\Entity\Login;
 use App\Model\Entity\ForgottenPassword;
+use App\Security\AccessManager;
 
 /**
  * Sending error reports to administrator by email.
@@ -24,13 +25,17 @@ class ForgottenPasswordHelper {
   /** @var string Prefix of mail subject to be used */
   private $subjectPrefix;
 
+  /** @var AccessManager */
+  private $accessManager;
+
   /**
    * Constructor
    * @param \App\Helpers\EmailHelper $emailHelper
    */
-  public function __construct(EntityManager $em, EmailHelper $emailHelper, array $params) {
+  public function __construct(EntityManager $em, EmailHelper $emailHelper, AccessManager $accessManager , array $params) {
     $this->em = $em;
     $this->emailHelper = $emailHelper;
+    $this->accessManager = $accessManager;
     $this->sender = Arrays::get($params, ["emails", "from"], "noreply@recodex.cz");
     $this->subjectPrefix = Arrays::get($params, ["emails", "subjectPrefix"], "ReCodEx Forgotten Password Request - ");
   }
@@ -40,21 +45,34 @@ class ForgottenPasswordHelper {
    * @param Login $login
    */
   public function process(Login $login, string $redirectUrl) {
-    $user = $login->user;
-    $email = $user->email;
-
-    // Save the report to the database
-    $entry = new ForgottenPassword($user, $email);
+    // Stalk forgotten password requests a little bit and store them to database
+    $entry = new ForgottenPassword($login->user, $login->user->email, $redirectUrl);
     $this->em->persist($entry);
     $this->em->flush();
+
+    // prepare all necessary things
+    $token = $this->accessManager->issueToken($login->user, [ "modify-password" ]);
+    $subject = $this->createSubject($login);
+    $message = $this->createBody($login, $redirectUrl, $token);
 
     // Send the mail
     return $this->emailHelper->send(
       $this->sender,
-      [ $user->email ],
-      $this->subjectPrefix,
-      $this->formatBody($message)
+      [ $login->user->email ],
+      $subject,
+      $message
     );
+  }
+
+  private function createSubject(Login $login): string {
+    return $this->subjectPrefix . " " . $login->username;
+  }
+
+  private function createBody(Login $login, string $redirectUrl, string $token): string {
+    $msg = "User " . $login->username . " requested password renewal.<br>";
+    $msg .= "Password can be changed after clicking on this ";
+    $msg .= "<a href=\"" . $redirectUrl . "?token=" . $token . "\">link</a>";
+    return $msg;
   }
 
 }
