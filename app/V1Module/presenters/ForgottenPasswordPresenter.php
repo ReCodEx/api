40251b273365b4c53e7e31677af480bda218cc3a
Simon Rozsival <simon@rozsival.com>
2016-10-19 20:20:51 +0200

    wip - change password

--- app/V1Module/presenters/ForgottenPasswordPresenter.php
+++ app/V1Module/presenters/ForgottenPasswordPresenter.php
@@ -2,8 +2,10 @@
 
 namespace App\V1Module\Presenters;
 
-use App\Model\Repository\Logins;
+use App\Exceptions\ForgottenRequestException;
 use App\Helpers\ForgottenPasswordHelper;
+use App\Model\Repository\Logins;
+use App\Security\AccessToken;
 
 class ForgottenPasswordPresenter extends BasePresenter {
 
@@ -22,16 +24,35 @@ class ForgottenPasswordPresenter extends BasePresenter {
   /**
    * @POST
    * @Param(type="post", name="username", validation="string:2..")
-   * @Param(type="post", name="redirectUrl", validation="string:2..")
    */
   public function actionDefault() {
     $req = $this->getHttpRequest();
     $username = $req->getPost("username");
-    $redirectUrl = $req->getPost("redirectUrl");
 
     // try to find login according to username and process request
     $login = $this->logins->findByUsernameOrThrow($username);
-    $this->forgottenPasswordHelper->process($login, $redirectUrl);
+    $this->forgottenPasswordHelper->process($login);
+
+    $this->sendSuccessResponse("OK");
+  }
+
+  /**
+   * @POST
+   * @Param(type="post", name="password", validation="string:2..")
+   * @LoggedIn
+   */
+  public function actionChange() {
+    $req = $this->getHttpRequest();
+
+    if (!$this->user->identity->token->isInScope(AccessToken::SCOPE_CHANGE_PASSWORD)) {
+      throw new ForbiddenRequestException("You cannot reset your password with this access token.");
+    }
+
+    // try to find login according to username and process request
+    $password = $req->getPost("password");
+    $login = $this->logins->findCurrent();
+
+    // @todo actually change the password
 
     $this->sendSuccessResponse("OK");
   }
