9ca0b116a1bd400946ed10347885d69d260a7984
Martin Polanka <PolankaMartin@gmail.com>
2016-10-19 13:18:13 +0200

    WIP: ForgottenPassword

---
+++ app/model/entity/ForgottenPassword.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Model\Entity;
+
+use \DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class ForgottenPassword
+{
+  use \Kdyby\Doctrine\Entities\MagicAccessors;
+
+  public function __construct(
+    User $user,
+    string $sentTo,
+    string $redirectUrl
+  ) {
+    $this->user = $user;
+    $this->sentTo = $sentTo;
+    $this->requestedAt = DateTime;
+    $this->redirectUrl = $redirectUrl;
+  }
+
+  /**
+   * @ORM\Id
+   * @ORM\Column(type="guid")
+   * @ORM\GeneratedValue(strategy="UUID")
+   */
+  protected $id;
+
+  /**
+   * @ORM\ManyToOne(targetEntity="User")
+   */
+  protected $user;
+
+  /**
+   * @ORM\Column(type="datetime")
+   */
+  protected $requestedAt;
+
+  /**
+   * @ORM\Column(type="string")
+   */
+  protected $sentTo;
+
+  /**
+   * @ORM\Column(type="string")
+   */
+  protected $redirectUrl;
+
+}
