8f193b65f9cb85c0df1f82e45bd4a2fce5015f03
Simon Rozsival <simon@rozsival.com>
2016-10-28 17:33:52 +0200

    hardware group - availability log wip

--- app/model/entity/HardwareGroup.php
+++ app/model/entity/HardwareGroup.php
@@ -4,6 +4,9 @@ namespace App\Model\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use JsonSerializable;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Criteria;
 
 /**
  * @ORM\Entity
@@ -23,12 +26,38 @@ class HardwareGroup implements JsonSerializable
    */
   protected $description;
 
+  /**
+   * @ORM\OneToMany(targetEntity="HardwareGroupAvailabilityLog", mappedBy="hardwareGroup")
+   * @ORM\OrderBy({ "loggedAt" = "DESC" })
+   */
+  protected $availabilityLog;
+
+  /**
+   * Find out whether the hardware group is available now or was available at a given time. 
+   * @param DateTime $when Explicit time
+   * @return bool
+   */
+  public function isAvailable(DateTime $when = NULL): bool {
+    if ($when === NULL) {
+      $when = new DateTime;
+    }
+
+    $criteria = Criteria::create()->where(Criteria::expr()->lte("loggedAt", $when));
+    $latestLog = $this->availabilityLog->matching($criteria)->first();
+    if (!$latestLog) {
+      return FALSE;
+    }
+
+    return $latestLog->isActive();
+  }
+
   public function __construct(
     string $id,
     string $description
   ) {
     $this->id = $id;
     $this->desciption = $description;
+    $this->availabilityLog = new ArrayCollection;
   }
 
   public function jsonSerialize() {
