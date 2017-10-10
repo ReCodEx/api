54b1972865817993e18a23bf5b3dac50dd66801d
Martin Polanka <PolankaMartin@gmail.com>
2017-10-10 22:14:34 +0200

    Missed comment

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: headers, Authorization, Accept-Language");
 
 // Uncomment this line if you must temporarily take down your site for maintenance.
-require __DIR__ . '/.maintenance.php';
+// require __DIR__ . '/.maintenance.php';
 
 $container = require __DIR__ . '/../app/bootstrap.php';
 $container->getByType(Nette\Application\Application::class)->run();
