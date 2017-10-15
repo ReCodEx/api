861d18c0f566161f08643a52e92d7286808111aa
Teyras <Teyras@gmail.com>
2017-10-15 22:59:40 +0200

    add documentation

--- app/helpers/Evaluation/EvaluationStatus/IEvaluable.php
+++ app/helpers/Evaluation/EvaluationStatus/IEvaluable.php
@@ -22,13 +22,21 @@ interface IEvaluable {
   function getEvaluation(): SolutionEvaluation;
 
   /**
-   * Query if evaluation is possible
+   * Query if evaluation is possible (it might not be possible e.g. if the backend rejected the request)
    * @return boolean The result
    */
   function canBeEvaluated(): bool;
 
+  /**
+   * Return true if the evaluation was finished successfully
+   * @return bool
+   */
   function isValid(): bool;
 
+  /**
+   * Return true if the evaluated object was marked as correct by the backend
+   * @return bool
+   */
   function isCorrect(): bool;
 
 }
