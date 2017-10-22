4fa3e67975d9e1961c77e1e2d5d1f27217cdc774
Martin Polanka <PolankaMartin@gmail.com>
2017-10-22 21:23:17 +0200

    Use UTC type for all datetimes in doctrine

---
+++ app/model/UTCDateTimeType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace DoctrineExtensions\DBAL\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\DateTimeType;
+
+/**
+ * Special type for storing and loading UTC datetime structures from database.
+ * Taken from: http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/cookbook/working-with-datetime.html
+ */
+class UTCDateTimeType extends DateTimeType
+{
+  private static $utc;
+  private static function getUtc() {
+    return self::$utc ? self::$utc : self::$utc = new \DateTimeZone('UTC');
+  }
+
+  public function convertToDatabaseValue($value, AbstractPlatform $platform) {
+    if ($value instanceof \DateTime) {
+      $value->setTimezone(self::getUtc());
+    }
+
+    return parent::convertToDatabaseValue($value, $platform);
+  }
+
+  public function convertToPHPValue($value, AbstractPlatform $platform) {
+    if (null === $value || $value instanceof \DateTime) {
+      return $value;
+    }
+
+    $converted = \DateTime::createFromFormat(
+      $platform->getDateTimeFormatString(),
+      $value,
+      self::getUtc()
+    );
+
+    if (! $converted) {
+      throw ConversionException::conversionFailedFormat(
+        $value,
+        $this->getName(),
+        $platform->getDateTimeFormatString()
+      );
+    }
+
+    return $converted;
+  }
+}
