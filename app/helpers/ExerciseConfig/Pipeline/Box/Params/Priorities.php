0bfcda2d90b19d08aad9f0a0847e5eda207483e3
Martin Polanka <PolankaMartin@gmail.com>
2017-11-02 15:28:08 +0100

    Tasks have default priority set to 0, optionally they can set it.

---
+++ app/helpers/ExerciseConfig/Pipeline/Box/Params/Priorities.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Helpers\ExerciseConfig\Pipeline\Box\Params;
+
+
+/**
+ * Some default priorities for boxes.
+ */
+class Priorities
+{
+  public static $INITIATION = 100;
+  public static $EXECUTION = 90;
+  public static $EVALUATION = 80;
+}
