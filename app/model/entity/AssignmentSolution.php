ff4ee41e081befdafe20f1f4b6586552f0efc3ad
Martin Polanka <PolankaMartin@gmail.com>
2017-11-19 13:37:21 +0100

    Return id of last submission in solution entity

--- app/model/entity/AssignmentSolution.php
+++ app/model/entity/AssignmentSolution.php
@@ -142,6 +142,10 @@ class AssignmentSolution implements JsonSerializable
    * @return array
    */
   public function getData($canViewRatios = FALSE, bool $canViewValues = false, bool $canViewResubmissions = false) {
+    $lastSubmissionId = $this->getLastSubmission() ? $this->getLastSubmission()->getId() : null;
+    $lastSubmissionIdArray = $lastSubmissionId ? [ $lastSubmissionId ] : [];
+    $submissions = $canViewResubmissions ? $this->getSubmissionsIds() : $lastSubmissionIdArray;
+
     return [
       "id" => $this->id,
       "note" => $this->note,
@@ -152,7 +156,7 @@ class AssignmentSolution implements JsonSerializable
       "accepted" => $this->accepted,
       "bonusPoints" => $this->bonusPoints,
       "lastSubmission" => $this->getLastSubmission() ?  $this->getLastSubmission()->getData($canViewRatios, $canViewValues) : null,
-      "submissions" => $canViewResubmissions ? $this->getSubmissionsIds() : []
+      "submissions" => $submissions
     ];
   }
 
