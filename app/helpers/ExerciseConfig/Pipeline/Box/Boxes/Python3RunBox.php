4aba204d97bf6e881c7200f4ea8d7d5b2e26216f
Martin Polanka <PolankaMartin@gmail.com>
2018-01-09 20:29:33 +0100

    Python compilation

---
+++ app/helpers/ExerciseConfig/Pipeline/Box/Boxes/Python3RunBox.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Helpers\ExerciseConfig\Pipeline\Box;
+
+use App\Exceptions\ExerciseConfigException;
+use App\Helpers\ExerciseConfig\Compilation\CompilationParams;
+use App\Helpers\ExerciseConfig\Pipeline\Box\Params\ConfigParams;
+use App\Helpers\ExerciseConfig\Pipeline\Ports\Port;
+use App\Helpers\ExerciseConfig\Pipeline\Ports\PortMeta;
+use App\Helpers\ExerciseConfig\VariableTypes;
+
+
+/**
+ * Box which represents execution python script.
+ */
+class Python3RunBox extends ExecutionBox
+{
+  /** Type key */
+  public static $PYTHON3_TYPE = "python3";
+  public static $PYTHON3_BINARY = "/usr/bin/python3";
+  public static $PYC_FILE_PORT_KEY = "pyc-file";
+  public static $DEFAULT_NAME = "Python3 Execution";
+
+  private static $initialized = false;
+  private static $defaultInputPorts;
+  private static $defaultOutputPorts;
+
+  /**
+   * Static initializer.
+   */
+  public static function init() {
+    if (!self::$initialized) {
+      self::$initialized = true;
+      self::$defaultInputPorts = array(
+        new Port((new PortMeta)->setName(self::$PYC_FILE_PORT_KEY)->setType(VariableTypes::$FILE_TYPE)),
+        new Port((new PortMeta)->setName(self::$EXECUTION_ARGS_PORT_KEY)->setType(VariableTypes::$STRING_ARRAY_TYPE)),
+        new Port((new PortMeta)->setName(self::$STDIN_FILE_PORT_KEY)->setType(VariableTypes::$FILE_TYPE)),
+        new Port((new PortMeta)->setName(self::$INPUT_FILES_PORT_KEY)->setType(VariableTypes::$FILE_ARRAY_TYPE))
+      );
+      self::$defaultOutputPorts = array(
+        new Port((new PortMeta)->setName(self::$STDOUT_FILE_PORT_KEY)->setType(VariableTypes::$FILE_TYPE)),
+        new Port((new PortMeta)->setName(self::$OUTPUT_FILE_PORT_KEY)->setType(VariableTypes::$FILE_TYPE))
+      );
+    }
+  }
+
+  /**
+   * ElfExecutionBox constructor.
+   * @param BoxMeta $meta
+   */
+  public function __construct(BoxMeta $meta) {
+    parent::__construct($meta);
+  }
+
+
+  /**
+   * Get type of this box.
+   * @return string
+   */
+  public function getType(): string {
+    return self::$PYTHON3_TYPE;
+  }
+
+  /**
+   * Get default input ports for this box.
+   * @return array
+   */
+  public function getDefaultInputPorts(): array {
+    self::init();
+    return self::$defaultInputPorts;
+  }
+
+  /**
+   * Get default output ports for this box.
+   * @return array
+   */
+  public function getDefaultOutputPorts(): array {
+    self::init();
+    return self::$defaultOutputPorts;
+  }
+
+  /**
+   * Get default name of this box.
+   * @return string
+   */
+  public function getDefaultName(): string {
+    return self::$DEFAULT_NAME;
+  }
+
+  /**
+   * Compile box into set of low-level tasks.
+   * @param CompilationParams $params
+   * @return array
+   * @throws ExerciseConfigException
+   */
+  public function compile(CompilationParams $params): array {
+    $task = $this->compileBaseTask($params);
+    $task->setCommandBinary(self::$PYTHON3_BINARY);
+
+    $args = [$this->getInputPortValue(self::$PYC_FILE_PORT_KEY)->getPrefixedValue(ConfigParams::$EVAL_DIR)];
+    if ($this->hasInputPortValue(self::$EXECUTION_ARGS_PORT_KEY)) {
+      $args = array_merge($args, $this->getInputPortValue(self::$EXECUTION_ARGS_PORT_KEY)->getValue());
+    }
+    $task->setCommandArguments($args);
+
+    return [$task];
+  }
+
+}
