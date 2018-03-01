d9d26ccd14b9617613286ff84ed1b9d285ac7f89
Martin Polanka <PolankaMartin@gmail.com>
2018-03-01 15:42:47 +0100

    Fix migration which deletes old reference solution

--- migrations/Version20180301094432.php
+++ migrations/Version20180301094432.php
@@ -34,6 +34,8 @@ class Version20180301094432 extends AbstractMigration
     $this->connection->executeQuery('ALTER TABLE reference_solution_submission ADD CONSTRAINT FK_AA9C8B99456C5646 FOREIGN KEY (evaluation_id) REFERENCES solution_evaluation (id) ON DELETE CASCADE');
     $this->connection->executeQuery('ALTER TABLE reference_solution_submission DROP FOREIGN KEY FK_62BA741FFA3CA3B7');
     $this->connection->executeQuery('ALTER TABLE reference_solution_submission ADD CONSTRAINT FK_62BA741FFA3CA3B7 FOREIGN KEY (reference_solution_id) REFERENCES reference_exercise_solution (id) ON DELETE CASCADE');
+    $this->connection->executeQuery('ALTER TABLE submission_failure DROP FOREIGN KEY FK_D7A9817B54DD80A');
+    $this->connection->executeQuery('ALTER TABLE submission_failure ADD CONSTRAINT FK_D7A9817B54DD80A FOREIGN KEY (reference_solution_submission_id) REFERENCES reference_solution_submission (id) ON DELETE CASCADE');
 
     // without any shame, lets delete all soft-deleted reference solutions
     $this->connection->executeQuery("DELETE FROM reference_exercise_solution WHERE deleted_at IS NOT NULL");
@@ -45,6 +47,8 @@ class Version20180301094432 extends AbstractMigration
     $this->connection->executeQuery('ALTER TABLE reference_solution_submission ADD CONSTRAINT FK_62BA741F456C5646 FOREIGN KEY (evaluation_id) REFERENCES solution_evaluation (id)');
     $this->connection->executeQuery('ALTER TABLE reference_solution_submission DROP FOREIGN KEY FK_62BA741FFA3CA3B7');
     $this->connection->executeQuery('ALTER TABLE reference_solution_submission ADD CONSTRAINT FK_62BA741FFA3CA3B7 FOREIGN KEY (reference_solution_id) REFERENCES reference_exercise_solution (id)');
+    $this->connection->executeQuery('ALTER TABLE submission_failure DROP FOREIGN KEY FK_D7A9817B54DD80A');
+    $this->connection->executeQuery('ALTER TABLE submission_failure ADD CONSTRAINT FK_D7A9817B54DD80A FOREIGN KEY (reference_solution_submission_id) REFERENCES reference_solution_submission (id)');
   }
 
   /**
