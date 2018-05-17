aa52c6599624cd7378a657c448a2d8bfc8e072e9
Teyras <Teyras@gmail.com>
2018-05-17 12:22:49 +0200

    Add permission hint generator

---
+++ app/helpers/PermissionHints.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Helpers;
+
+use Nette\Reflection\ClassType;
+use Nette\Reflection\Method;
+use Nette\StaticClassException;
+use Nette\Utils\Strings;
+
+class PermissionHints {
+  public function __construct() {
+    throw new StaticClassException();
+  }
+
+  public static function generate($aclModule, $subject) {
+    $reflection = ClassType::from($aclModule);
+    foreach ($reflection->getMethods(Method::IS_PUBLIC) as $method) {
+      if (!Strings::startsWith($method->getName(), "can")) {
+        continue;
+      }
+
+      if ($method->getNumberOfRequiredParameters() > 1 || $method->getNumberOfParameters() === 0) {
+        continue;
+      }
+
+      $parameter = $method->getParameters()[0];
+      $className = $parameter->getClass() ? $parameter->getClass()->getName() : null;
+      if ($className !== null && $subject instanceof $className) {
+        yield $method->getName() => $method->invoke($aclModule, $subject);
+      }
+    }
+  }
+
+  public static function get($aclModule, $subject) {
+    return iterator_to_array(static::generate($aclModule, $subject));
+  }
+}
