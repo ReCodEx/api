966d3d50351a1f252afc1428d187784867d5320a
Martin Polanka <PolankaMartin@gmail.com>
2018-08-19 14:40:32 +0200

    Base description of optimisation

--- app/helpers/ExerciseConfig/Compilation/BoxesOptimizer.php
+++ app/helpers/ExerciseConfig/Compilation/BoxesOptimizer.php
@@ -15,8 +15,32 @@ use App\Helpers\ExerciseConfig\Compilation\Tree\RootedTree;
 class BoxesOptimizer {
 
   /**
+   * In-place optimisation of the given tree.
+   * @param RootedTree $tree
+   */
+  private function optimizeTree(RootedTree $tree) {
+    // Ok, here it goes...
+    // The whole optimisation is based on following heuristic. We were given a
+    // rooted tree which can have multiple root nodes. The tree is traversed by
+    // levels and can be implemented with recursion. At first all root nodes are
+    // compared if there are any duplicates. If duplicates are found, then these
+    // nodes are merged into one. The next step is to process all subtrees which
+    // were created. Thus if there were 4 nodes and 2 and 2 are the same, these
+    // four nodes are contracted into 2 nodes. These two nodes then contain
+    // subtrees from the 2 nodes of which they are composed. After this,
+    // the procedure is repeated for all nodes from subtrees.
+    // Therefore this heuristics is capable only optimise the begging of the
+    // trees and not the ends. This is generally fine for our usage, because
+    // usually the same thing for all tests is compilation which is the first
+    // set of tasks in the tree.
+
+    // TODO: implementation
+  }
+
+  /**
    * Optimize given array of boxes in tests and remove duplicate boxes.
-   * Resulting array will be multidimensional sort-of tree.
+   * The optimizer should return RootedTree which should be similar to given
+   * trees.
    * @param RootedTree[] $tests
    * @return RootedTree
    */
@@ -27,6 +51,14 @@ class BoxesOptimizer {
         $tree->addRootNode($rootNode);
       }
     }
+
+    // The hell of optimisation awaits...
+    // Forth, and fear no darkness! Arise! Arise, Riders of ReCodEx! Spears
+    // shall be shaken, shields shall be splintered! A sword day... a red day...
+    // ere the sun rises!
+    // Optimize! Optimize! OPTIMIZE!
+
+    $this->optimizeTree($tree);
     return $tree;
   }
 
