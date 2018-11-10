5ae36689b3b4f937a2f375dbe0dbd8c19a21d8a7
Martin Polanka <PolankaMartin@gmail.com>
2018-11-10 13:13:29 +0100

    Notifications presenter implementation

--- app/V1Module/security/ACL/INotificationPermissions.php
+++ app/V1Module/security/ACL/INotificationPermissions.php
@@ -2,10 +2,16 @@
 
 namespace App\Security\ACL;
 
+use App\Model\Entity\Group;
 use App\Model\Entity\Notification;
 
 interface INotificationPermissions {
   function canViewAll(): bool;
   function canViewCurrent(): bool;
   function canViewDetail(Notification $notification): bool;
+  function canCreate(): bool;
+  function canCreateGlobal(): bool;
+  function canAddGroup(Group $group): bool;
+  function canUpdate(Notification $notification): bool;
+  function canRemove(Notification $notification): bool;
 }
