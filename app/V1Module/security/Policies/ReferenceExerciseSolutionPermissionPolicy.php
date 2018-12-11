b7b8a91eb65e38c119a42ea10eb1636c4e248f80
Martin Krulis <krulis@ksi.mff.cuni.cz>
2018-12-12 00:54:40 +0100

    Updating permission so regular teachers may resubmit ref. solutions for the same exercises for which they can submit ref. solutions of their own.

--- app/V1Module/security/Policies/ReferenceExerciseSolutionPermissionPolicy.php
+++ app/V1Module/security/Policies/ReferenceExerciseSolutionPermissionPolicy.php
@@ -53,4 +53,24 @@ class ReferenceExerciseSolutionPermissionPolicy implements IPermissionPolicy {
 
     return false;
   }
+
+  public function isExerciseSubGroupSupervisor(Identity $identity, ReferenceExerciseSolution $referenceExerciseSolution)
+  {
+    $user = $identity->getUserData();
+    $exercise = $referenceExerciseSolution->getExercise();
+
+    if ($user === null || $exercise->getGroups()->isEmpty() ||
+        $exercise->isPublic() === false) {
+      return false;
+    }
+
+    /** @var Group $group */
+    foreach ($exercise->getGroups() as $group) {
+      if ($group->isAdminOrSupervisorOfSubgroup($user)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
