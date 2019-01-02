7ec8fe8345494659a576b07b60f52473ece8a6dd
Martin Polanka <PolankaMartin@gmail.com>
2019-01-02 14:39:21 +0100

    Shadow assignment statistics in group stats should contain even assignments with no awarded points

--- app/model/repository/ShadowAssignmentPointsRepository.php
+++ app/model/repository/ShadowAssignmentPointsRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Model\Repository;
 
+use App\Helpers\Pair;
 use App\Model\Entity\ShadowAssignment;
 use App\Model\Entity\ShadowAssignmentPoints;
 use App\Model\Entity\User;
@@ -20,13 +21,26 @@ class ShadowAssignmentPointsRepository extends BaseRepository {
    * Find points for given user of given assignments.
    * @param ShadowAssignment[] $shadowAssignments
    * @param User $user
-   * @return ShadowAssignmentPoints[]
+   * @return Pair[] indexed by the shadow assignment identification, value is
+   * pair which key is assignment and value is points. Points can be null if
+   * there are no points for the assignment and user
    */
   public function findPointsForAssignments(array $shadowAssignments, User $user): array {
-    return $this->findBy([
+    $result = [];
+    foreach ($shadowAssignments as $assignment) {
+      $result[$assignment->getId()] = new Pair($assignment, null);
+    }
+
+    $pointsList = $this->findBy([
       "author" => $user,
       "shadowAssignment" => $shadowAssignments, // doctrine will handle given array with IN operator
     ]);
+
+    /** @var ShadowAssignmentPoints $points */
+    foreach ($pointsList as $points) {
+      $result[$points->getShadowAssignment()->getId()]->value = $points;
+    }
+    return $result;
   }
 
 }
