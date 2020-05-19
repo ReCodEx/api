697e7472a135e70fb38e9487858b0dc672a13876
Martin Krulis <krulis@ksi.mff.cuni.cz>
2020-05-20 00:44:23 +0200

    Fixing exercise limits validation (missing tests in the limits config are reported).

--- app/helpers/ExerciseConfig/Validation/ExerciseLimitsValidator.php
+++ app/helpers/ExerciseConfig/Validation/ExerciseLimitsValidator.php
@@ -40,12 +40,20 @@ class ExerciseLimitsValidator
      */
     public function validate(Exercise $exercise, HwGroupMeta $hwGroupMeta, ExerciseLimits $exerciseLimits)
     {
-        $exerciseTests = $exercise->getExerciseTestsIds();
+        $exerciseTests = $exercise->getExerciseTestsNames();
+        $limits = $exerciseLimits->getLimitsArray();
+        foreach ($exerciseTests as $testId => $testName) {
+            if (!array_key_exists($testId, $limits)) {
+                throw new ExerciseConfigException(
+                    sprintf("Test '%s' does not have any limits specified", $testName)
+                );
+            }
+        }
 
         $cpuTimeSum = 0;
         $wallTimeSum = 0;
-        foreach ($exerciseLimits->getLimitsArray() as $testId => $testLimits) {
-            if (!in_array($testId, $exerciseTests)) {
+        foreach ($limits as $testId => $testLimits) {
+            if (!array_key_exists($testId, $exerciseTests)) {
                 throw new ExerciseConfigException(
                     sprintf(
                         "Test with id '%s' is not present in the exercise configuration",
@@ -55,12 +63,12 @@ class ExerciseLimitsValidator
             }
 
             if ($testLimits->getMemoryLimit() === 0) {
-                throw new ExerciseConfigException(sprintf("Test with id '%s' needs to have a memory limit", $testId));
+                throw new ExerciseConfigException(sprintf("Test '%s' needs to have a memory limit", $exerciseTests[$testId]));
             }
 
             if ($testLimits->getCpuTime() === 0.0 && $testLimits->getWallTime() === 0.0) {
                 throw new ExerciseConfigException(
-                    sprintf("Test with id '%s' needs to have a time limit (either cpu or wall)", $testId)
+                    sprintf("Test '%s' needs to have a time limit (either cpu or wall)", $exerciseTests[$testId])
                 );
             }
 
@@ -69,15 +77,15 @@ class ExerciseLimitsValidator
 
             if ($testLimits->getMemoryLimit() > $hwGroupMeta->getMemory()) {
                 throw new ExerciseConfigException(
-                    sprintf("Test with id '%s' has exceeded memory limit '%d'", $testId, $hwGroupMeta->getMemory())
+                    sprintf("Test '%s' has exceeded memory limit '%d'", $exerciseTests[$testId], $hwGroupMeta->getMemory())
                 );
             }
 
             if ($testLimits->getCpuTime() > $hwGroupMeta->getCpuTimePerTest()) {
                 throw new ExerciseConfigException(
                     sprintf(
-                        "Test with id '%s' has exceeded cpu time limit '%d'",
-                        $testId,
+                        "Test '%s' has exceeded cpu time limit '%d'",
+                        $exerciseTests[$testId],
                         $hwGroupMeta->getCpuTimePerTest()
                     )
                 );
@@ -86,8 +94,8 @@ class ExerciseLimitsValidator
             if ($testLimits->getWallTime() > $hwGroupMeta->getWallTimePerTest()) {
                 throw new ExerciseConfigException(
                     sprintf(
-                        "Test with id '%s' has exceeded wall time limit '%d'",
-                        $testId,
+                        "Test '%s' has exceeded wall time limit '%d'",
+                        $exerciseTests[$testId],
                         $hwGroupMeta->getWallTimePerTest()
                     )
                 );
