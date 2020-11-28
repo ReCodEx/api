f8f19e4ece8bb4a47441c66efb51ea9666a5b07c
Martin Krulis <krulis@ksi.mff.cuni.cz>
2020-11-28 20:04:32 +0100

    Improving submission notifications in case a solution is re-evaluated.

--- app/helpers/Emails/Notifications/Submissions/SubmissionEmailsSender.php
+++ app/helpers/Emails/Notifications/Submissions/SubmissionEmailsSender.php
@@ -77,11 +77,26 @@ class SubmissionEmailsSender
             return false;
         }
 
+        if (!$solution->getLastSubmission() || $solution->getLastSubmission()->getId() !== $submission->getId()) {
+            // another submission (reevaluation) of the same solution has already been made
+            return false;
+        }
+
+        $isResubmit = count($solution->getSubmissions()) > 1;
+
         // Handle evaluation notifications...
-        $resultSubmissionEvaluated = $this->handleSubmissionEvaluated($solution, $submission, $user);
-        $resultSubmissionAfterAcceptance = $this->handleSubmissionAfterAcceptance($solution, $submission, $user);
-        $resultSubmissionAfterReview = $this->handleSubmissionAfterReview($solution, $submission, $user);
-        return $resultSubmissionEvaluated && $resultSubmissionAfterAcceptance && $resultSubmissionAfterReview;
+        $resultSubmissionEvaluated = $this->handleSubmissionEvaluated($solution, $submission, $user, $isResubmit);
+        
+        if ($isResubmit) {
+            // no other notifications in case of resubmits
+            return $resultSubmissionEvaluated;
+        } else {
+            // review-related notifications are actually bound with solution
+            // (i.e., relevant only when it is evaluated for the first time)
+            $resultSubmissionAfterAcceptance = $this->handleSubmissionAfterAcceptance($solution, $submission, $user);
+            $resultSubmissionAfterReview = $this->handleSubmissionAfterReview($solution, $submission, $user);
+            return $resultSubmissionEvaluated && $resultSubmissionAfterAcceptance && $resultSubmissionAfterReview;
+        }
     }
 
     /**
@@ -90,15 +105,21 @@ class SubmissionEmailsSender
     private function handleSubmissionEvaluated(
         AssignmentSolution $solution,
         AssignmentSolutionSubmission $submission,
-        User $user
+        User $user,
+        bool $isResubmit
     ): bool {
         $threshold = (new DateTime())->modify($this->submissionNotificationThreshold);
+        $createdAt = $solution->getSolution()->getCreatedAt();
         if (
-            $user->getSettings()->getSubmissionEvaluatedEmails() && $solution->getSolution()->getCreatedAt(
-            ) < $threshold
+            $user->getSettings()->getSubmissionEvaluatedEmails() && ($createdAt < $threshold || $isResubmit)
         ) {
             $locale = $user->getSettings()->getDefaultLanguage();
-            $result = $this->createSubmissionEvaluated($solution->getAssignment(), $submission, $locale);
+            $result = $this->createSubmissionEvaluated(
+                $solution->getAssignment(),
+                $submission,
+                $isResubmit,
+                $locale
+            );
 
             // Send the mail
             return $this->emailHelper->send(
@@ -230,6 +251,7 @@ class SubmissionEmailsSender
      * Prepare and format body of the mail
      * @param Assignment $assignment
      * @param AssignmentSolutionSubmission $submission
+     * @param bool $isResubmit
      * @param string $locale
      * @return EmailRenderResult
      * @throws InvalidStateException
@@ -237,11 +259,13 @@ class SubmissionEmailsSender
     private function createSubmissionEvaluated(
         Assignment $assignment,
         AssignmentSolutionSubmission $submission,
+        bool $isResubmit,
         string $locale
     ): EmailRenderResult {
         // render the HTML to string using Latte engine
         $latte = EmailLatteFactory::latte();
         $template = EmailLocalizationHelper::getTemplate($locale, __DIR__ . "/submissionEvaluated_{locale}.latte");
+        $submittedBy = $submission->getSubmittedBy();
         return $latte->renderEmail(
             $template,
             [
@@ -264,6 +288,8 @@ class SubmissionEmailsSender
                         "solutionId" => $submission->getAssignmentSolution()->getId(),
                     ]
                 ),
+                "isResubmit" => $isResubmit,
+                "submittedBy" => $submittedBy ? $submittedBy->getName() : "",
             ]
         );
     }
