7f3bd8a4ebc4705fd73d12700b45bf015c117737
Martin Polanka <PolankaMartin@gmail.com>
2021-01-02 14:17:12 +0100

    Make export of fixtures work after nelmi/alice upgrade

--- app/commands/DoctrineFixtures.php
+++ app/commands/DoctrineFixtures.php
@@ -99,7 +99,7 @@ class DoctrineFixtures extends Command
             $groupFiles = [];
 
             /** @var SplFileInfo $file */
-            foreach (Finder::findFiles("*.neon", "*.yml", "*.yaml")->in($fixtureDir . "/" . $group) as $file) {
+            foreach (Finder::findFiles("*.neon", "*.yml", "*.yaml", "*.json")->in($fixtureDir . "/" . $group) as $file) {
                 $groupFiles[] = $file->getRealPath();
             }
 
