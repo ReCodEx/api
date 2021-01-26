f31e0838a5c0c3b456984808f66f0950ff79ee94
Martin Krulis <krulis@ksi.mff.cuni.cz>
2021-01-26 21:39:10 +0100

    Adding optional noAdmin flag to add group endpoint which prevents assigning the logged-in user as admin of newly created groups.

--- app/V1Module/presenters/GroupsPresenter.php
+++ app/V1Module/presenters/GroupsPresenter.php
@@ -162,16 +162,28 @@ class GroupsPresenter extends BasePresenter
     /**
      * Create a new group
      * @POST
-     * @Param(type="post", name="instanceId", validation="string:36", description="An identifier of the instance where the group should be created")
-     * @Param(type="post", name="externalId", required=false, description="An informative, human readable identifier of the group")
-     * @Param(type="post", name="parentGroupId", validation="string:36", required=false, description="Identifier of the parent group (if none is given, a top-level group is created)")
-     * @Param(type="post", name="publicStats", validation="bool", required=false, description="Should students be able to see each other's results?")
-     * @Param(type="post", name="detaining", validation="bool", required=false, description="Are students prevented from leaving the group on their own?")
-     * @Param(type="post", name="isPublic", validation="bool", required=false, description="Should the group be visible to all student?")
-     * @Param(type="post", name="isOrganizational", validation="bool", required=false, description="Whether the group is organizational (no assignments nor students).")
-     * @Param(type="post", name="localizedTexts", validation="array", required=false, description="Localized names and descriptions")
-     * @Param(type="post", name="hasThreshold", validation="bool", description="True if threshold was given, false if it should be unset")
-     * @Param(type="post", name="threshold", validation="numericint", required=false, description="A minimum percentage of points needed to pass the course")
+     * @Param(type="post", name="instanceId", validation="string:36",
+     *        description="An identifier of the instance where the group should be created")
+     * @Param(type="post", name="externalId", required=false,
+     *        description="An informative, human readable identifier of the group")
+     * @Param(type="post", name="parentGroupId", validation="string:36", required=false,
+     *        description="Identifier of the parent group (if none is given, a top-level group is created)")
+     * @Param(type="post", name="publicStats", validation="bool", required=false,
+     *        description="Should students be able to see each other's results?")
+     * @Param(type="post", name="detaining", validation="bool", required=false,
+     *        description="Are students prevented from leaving the group on their own?")
+     * @Param(type="post", name="isPublic", validation="bool", required=false,
+     *        description="Should the group be visible to all student?")
+     * @Param(type="post", name="isOrganizational", validation="bool", required=false,
+     *        description="Whether the group is organizational (no assignments nor students).")
+     * @Param(type="post", name="localizedTexts", validation="array", required=false,
+     *        description="Localized names and descriptions")
+     * @Param(type="post", name="hasThreshold", validation="bool",
+     *        description="True if threshold was given, false if it should be unset")
+     * @Param(type="post", name="threshold", validation="numericint", required=false,
+     *        description="A minimum percentage of points needed to pass the course")
+     * @Param(type="post", name="noAdmin", validation="bool", required=false,
+     *        description="If true, no admin is assigned to group (current user is assigned as admin by default.")
      * @throws ForbiddenRequestException
      * @throws InvalidArgumentException
      */
@@ -200,8 +212,18 @@ class GroupsPresenter extends BasePresenter
         $isPublic = filter_var($req->getPost("isPublic"), FILTER_VALIDATE_BOOLEAN);
         $isOrganizational = filter_var($req->getPost("isOrganizational"), FILTER_VALIDATE_BOOLEAN);
         $hasThreshold = filter_var($req->getPost("hasThreshold"), FILTER_VALIDATE_BOOLEAN);
+        $noAdmin = filter_var($req->getPost("noAdmin"), FILTER_VALIDATE_BOOLEAN);
 
-        $group = new Group($externalId, $instance, $user, $parentGroup, $publicStats, $isPublic, $isOrganizational, $detaining);
+        $group = new Group(
+            $externalId,
+            $instance,
+            $noAdmin ? null : $user,
+            $parentGroup,
+            $publicStats,
+            $isPublic,
+            $isOrganizational,
+            $detaining
+        );
         if ($hasThreshold) {
             $threshold = $req->getPost("threshold") !== null
                 ? $req->getPost("threshold") / 100
