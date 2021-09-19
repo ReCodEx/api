e92645328d929729eb59deffa81670515335c615
Martin Krulis <krulis@d3s.mff.cuni.cz>
2021-09-19 19:44:59 +0200

    Renaming "degrees" to "titles" in the user entity.

---
+++ migrations/Version20210919173703.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210919173703 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `user` CHANGE degrees_before_name titles_before_name VARCHAR(255) NOT NULL, CHANGE degrees_after_name titles_after_name VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `user` CHANGE titles_before_name degrees_before_name VARCHAR(255) NOT NULL, CHANGE titles_after_name degrees_after_name VARCHAR(255) NOT NULL');
+    }
+}
