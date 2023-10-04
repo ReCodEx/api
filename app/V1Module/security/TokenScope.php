f378a8ab3d25ab5f7e135e2f3fefc766c9bbaa45
Martin Krulis <krulis@d3s.mff.cuni.cz>
2023-10-04 13:34:32 +0200

    Creating new permission scope for working with reference solutions.

--- app/V1Module/security/TokenScope.php
+++ app/V1Module/security/TokenScope.php
@@ -4,14 +4,46 @@ namespace App\Security;
 
 use Nette\StaticClass;
 
+/**
+ * Namespace for scope constants.
+ */
 class TokenScope
 {
     use StaticClass;
 
-    public const CHANGE_PASSWORD = "change-password";
+    /**
+     * The default scope with no additional restrictions.
+     */
     public const MASTER = "master";
-    public const EMAIL_VERIFICATION = "email-verification";
-    public const REFRESH = "refresh";
+
+    /**
+     * Read-only scope restricts operations to data retrieval only.
+     */
     public const READ_ALL = "read-all";
+
+    /**
+     * Used by 3rd party plagiarism detection tools to fetch solutions and feed similarities back.
+     */
     public const PLAGIARISM = "plagiarism";
+
+    /**
+     * Operations with reference solutions only. Can be used to insert additional solutions (e.g., created by GPT),
+     * as reference solutions to exercises.
+     */
+    public const REF_SOLUTIONS = "ref-solutions";
+
+    /**
+     * Special scope used in password-retrieval links. The user can only change the local password.
+     */
+    public const CHANGE_PASSWORD = "change-password";
+
+    /**
+     * Special scope used in password verification links. The user can only mark email address verified.
+     */
+    public const EMAIL_VERIFICATION = "email-verification";
+
+    /**
+     * Usually used in combination with other scopes. Allows refreshing the token.
+     */
+    public const REFRESH = "refresh";
 }
