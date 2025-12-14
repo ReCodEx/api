703b1a9ea6babdce4b56024c01a8d90d7434a8c8
Martin Kruliš <krulis@d3s.mff.cuni.cz>
2025-12-14 19:36:50 +0100

    Removing unimportant constraint that causes trouble.

---
+++ migrations/Version20251214183529.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251214183529 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_1187F7758A90ABA9D19302F8 ON exercise_file_link');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1187F7758A90ABA9D19302F8 ON exercise_file_link (`key`, assignment_id)');
+    }
+}
